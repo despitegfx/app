098cfd8a002cf0466eb71154dbba2e19e77fd148
despitegfx <despitegfx@gmail.com>
2020-11-19 13:26:35 +0000

    api interaction added

--- app/server.js
+++ app/server.js
@@ -4,13 +4,14 @@ const app = express();
 
 // Serve static files....
 app.use(express.static(__dirname));
-// app.use(express.static(__dirname + 'dist/app'));
+//app.use(express.static(__dirname + 'dist/app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    // res.sendFile(path.join(__dirname + 'dist/app/index.html'));
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
+    //res.sendFile(path.join(__dirname + 'dist/app/index.html'));
 });
 
-// default Heroku PORT
+
+//default Heroku PORT
 app.listen(process.env.PORT || 8080);
--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  //@ViewChild ('f') loginForm: NgForm;
+
+  // hey: string = "https://trade-client-connectivity.herokuapp.com/clients/all";
+
+  constructor( private http: HttpClient ) { 
+  //   this.http.get(this.hey).toPromise().then(data => {console.log(data);});
+  // }
+
+  this.http.get( 'https://trade-client-connectivity.herokuapp.com/clients/all').subscribe(responseData =>{
+  console.log(responseData);
+  });
+
 }
